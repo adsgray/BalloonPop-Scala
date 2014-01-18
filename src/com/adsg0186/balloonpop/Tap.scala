321924b3aa7b67cd252b6afea4dbdf3f5a978188
Andrew Gray <adsgray@gmail.com>
2014-01-17 21:40:50 -0800

    collisions but ugly vars

--- src/com/adsg0186/balloonpop/Tap.scala
+++ src/com/adsg0186/balloonpop/Tap.scala
@@ -14,45 +14,67 @@ import com.github.adsgray.gdxtry1.engine.util.PathFactory
 import android.util.Log
 
 object tapCollisionTrigger extends BlobTrigger {
-  override def trigger(source:BlobIF, secondary:BlobIF):BlobIF = {
+  override def trigger(source: BlobIF, secondary: BlobIF): BlobIF = {
     Log.d("trace", "Tap collided with a balloon!")
-    //val tap = source.asInstanceOf[TapBlob]
-    //tap.popped += 1
+    Tap.collision
+    // also make secondary go away here:
+    // replaceWithExplosion(secondary)
     source
   }
 }
 
 // triggered when TapBlob is finished
 object onTapComplete extends BlobTrigger {
-  override def trigger(source:BlobIF, secondary:BlobIF):BlobIF = {
+  override def trigger(source: BlobIF, secondary: BlobIF): BlobIF = {
     Log.d("trace", "Tap done")
-    val tap = source.asInstanceOf[TapBlob]
-    // tell some kind of game controller how many balloons this Tap popped
-    // for the purpose of score/bonus calculation
     // play a sound?
+    Tap.remove
     source
   }
 }
 
-
-case class TapBlob(b:BlobIF) extends BlobDecorator(b) {
+case class TapBlob(b: BlobIF) extends BlobDecorator(b) {
   var popped = 0
 }
 
 object Tap {
-  
-  val extentRadius = 50
-  val lifeTime = 20 // half a second?
 
-  def apply(pos:BlobPosition, w: WorldIF, r: Renderer):BlobIF = {
+  val extentRadius = 50
+  val lifeTime = 12 // half a second?
+  var currentTap: Option[TapBlob] = None
+
+  def apply(pos: BlobPosition, w: WorldIF, r: Renderer): BlobIF = {
+    currentTap match {
+      case Some(t) =>
+        Log.d("trace", "currentTap still exists")
+        currentTap.get
+      case None => createNewTapBlob(pos, w, r)
+    }
+  }
+
+  def remove = {
+    // tell some kind of game controller how many balloons this Tap popped
+    // for the purpose of score/bonus calculation
+    currentTap map { t =>
+      Log.d("trace", s"tap got ${t.popped} balloons")
+    }
+    currentTap = None
+  }
+
+  def collision = currentTap map { t => t.popped += 1 }
+
+  def createNewTapBlob(pos: BlobPosition, w: WorldIF, r: Renderer): BlobIF = {
     //public static BlobIF circleBlob(PositionIF p, BlobPath path, CircleConfig rc, Renderer r) {
     val rc = new r.CircleConfig(Color.WHITE, 1)
     val b = BlobFactory.circleBlob(pos, PathFactory.stationary, rc, r)
     b.setExtent(new CircleExtent(extentRadius))
+    b.setLifeTime(lifeTime)
     b.setWorld(w)
     b.registerCollisionTrigger(tapCollisionTrigger)
+    b.registerTickDeathTrigger(onTapComplete)
     val tap = TapBlob(b)
     w.addMissileToWorld(tap)
+    currentTap = Some(tap)
     tap
   }
 
