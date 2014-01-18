7c1959a52f124c8efda92edd5787c09b74a1a839
Andrew Gray <adsgray@gmail.com>
2014-01-17 18:12:13 -0800

    add balloon clusters

--- src/com/adsg0186/balloonpop/Balloons.scala
+++ src/com/adsg0186/balloonpop/Balloons.scala
@@ -1,18 +1,19 @@
 package com.adsg0186.balloonpop
 
 import java.util.Random
-
 import com.github.adsgray.gdxtry1.engine.blob.BlobIF
 import com.github.adsgray.gdxtry1.engine.blob.decorator.BlobDecorator
 import com.github.adsgray.gdxtry1.engine.extent.CircleExtent
 import com.github.adsgray.gdxtry1.engine.output.Renderer
+import com.github.adsgray.gdxtry1.engine.position.BlobPosition
 import com.github.adsgray.gdxtry1.engine.util.BlobFactory
 import com.github.adsgray.gdxtry1.engine.util.GameFactory
 import com.github.adsgray.gdxtry1.engine.util.PathFactory
+import com.github.adsgray.gdxtry1.engine.BlobCluster
 import com.github.adsgray.gdxtry1.engine.util.PositionFactory
 
 // Balloons are worth points when you pop them
-trait Balloon {
+trait Balloon extends BlobDecorator {
   def points
   def reactToPop = {
     // replace with explosion animation
@@ -30,34 +31,35 @@ trait asteroidBalloonTrait extends Balloon {
   }
 }
 
-case class SmallBalloon(b:BlobIF) extends BlobDecorator(b) with Balloon {
+case class SmallBalloon(b: BlobIF) extends BlobDecorator(b) with Balloon {
   override def points = 20
 }
-case class LargeBalloon(b:BlobIF) extends BlobDecorator(b) with Balloon {
+case class LargeBalloon(b: BlobIF) extends BlobDecorator(b) with Balloon {
   override def points = 10
 }
-case class AsteroidBalloon(b:BlobIF) extends BlobDecorator(b) with asteroidBalloonTrait {
+case class AsteroidBalloon(b: BlobIF) extends BlobDecorator(b) with asteroidBalloonTrait {
   override def points = 15
 }
 
-
 object Balloons {
   val rnd = new Random
-
-  var renderer:Option[Renderer] = null
-  def setRenderer(r:Renderer) = renderer = Some(r)
+  val maxXPos = 50
+  val maxYPos = 50
+  
+  var renderer: Option[Renderer] = null
+  def setRenderer(r: Renderer) = renderer = Some(r)
   def balloonColor = GameFactory.randomColor
 
-  // TODO: make these random-ish:
+  // These are always relative to a cluster:
   def balloonPosition = {
-    PositionFactory.origin
+    val xpos = rnd.nextInt(2 * maxXPos) - maxXPos
+    val ypos = rnd.nextInt(2 * maxYPos) - maxYPos
+    new BlobPosition(xpos, ypos)
   }
 
-  def balloonPath = {
-    PathFactory.stationary
-  }
+  def balloonPath = BalloonPath.chooseClosedPath
 
-  def balloonBlob:BlobIF = {
+  def balloonBlob: BlobIF = {
     val r = renderer.get
     val size = smallBalloonSize
     val rc = new r.CircleConfig(balloonColor, size)
@@ -67,10 +69,10 @@ object Balloons {
   }
 
   def smallBalloonSize = 20
-  def smallBalloon:Balloon = SmallBalloon(balloonBlob)
+  def smallBalloon: Balloon = SmallBalloon(balloonBlob)
 
   def largeBalloonSize = 60
-  def largeBalloon:Balloon = {
+  def largeBalloon: Balloon = {
     val r = renderer.get
     val b = balloonBlob
     val size = largeBalloonSize
@@ -79,15 +81,67 @@ object Balloons {
     b.setExtent(new CircleExtent(size))
     LargeBalloon(b)
   }
-  
+
   // get a largeBalloon and re-wrap it as an AsteroidBalloon
   // also make it rainbow-y
-  def asteroidBalloon:Balloon = largeBalloon match {
+  def asteroidBalloon: Balloon = largeBalloon match {
     case LargeBalloon(b) => AsteroidBalloon(BlobFactory.rainbowColorCycler(b, 10))
   }
 
   // TODO: allow a small chance of asteroidBalloon
   // might need a map...
-  def coinFlip = rnd.nextInt(100) < 50
-  def randomBalloon:Balloon = if (coinFlip) smallBalloon else largeBalloon
+
+  val balloonChoices = List[() => Balloon](
+    smallBalloon _,
+    largeBalloon _,
+    asteroidBalloon _)
+
+  def randomBalloon: Balloon = balloonChoices(rnd.nextInt(balloonChoices.size))()
+  
+}
+
+object BalloonPath {
+
+  def speed = 10
+  def interval = 5
+
+  // paths that loop
+  val closedPaths = List(
+    PathFactory.backAndForth(speed, interval),
+    PathFactory.backAndForthLeft(speed, interval),
+    PathFactory.squarePath(speed, interval),
+    PathFactory.squarePathClockwise(speed, interval),
+    PathFactory.upperTriangle(speed, interval),
+    PathFactory.upAndDown(speed, interval))
+
+  // choose one of the above paths randomly
+  def chooseClosedPath = closedPaths(Balloons.rnd.nextInt(closedPaths.size))
+
+}
+
+object BalloonCluster {
+  
+  // TODO choose cluster position and path randomly
+  def position = {
+    PositionFactory.origin
+  }
+  
+  def path = {
+    PathFactory.stationary
+  }
+
+  def balloonCluster(num:Int):BlobIF = {
+    val r = Balloons.renderer.get
+    val cluster = new BlobCluster(position, path, r)
+    // add num blobs to cluster
+    (1 to num) map { i =>
+      cluster.absorbBlob(Balloons.randomBalloon)
+    }
+    cluster
+  }
+  
+  val clusterSizes = List(1,2,3,4,5)
+  def randomCluster:BlobIF = {
+    balloonCluster(clusterSizes(Balloons.rnd.nextInt(clusterSizes.size)))
+  }
 }
