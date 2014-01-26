53f5d9275d6c9c6c79cb717fd6d9b4196c00c4b6
Andrew Gray <adsgray@gmail.com>
2014-01-26 08:57:20 -0800

    preferences stuff

--- src/com/adsg0186/balloonpop/GamePreferences.scala
+++ src/com/adsg0186/balloonpop/GamePreferences.scala
@@ -1,21 +1,63 @@
 package com.adsg0186.balloonpop
 
+import android.content.Context
+import android.content.SharedPreferences
+import android.preference.PreferenceManager
+
+case class Preferences(sound:Boolean, vibrate:Int)
+
 object GamePreferences {
 
+  protected var store:Option[SharedPreferences] = None
+  protected var cache:Option[Preferences] = None
+
+  def init(c:Context) = { store = Some(PreferenceManager.getDefaultSharedPreferences(c)) }
+
+  // return cached preferences
+  def get = { 
+    cache match {
+      case Some(p) => p
+      case None =>
+        cache = Some(Preferences(getSound, getVibrate)) 
+        cache.get
+    }
+  }
+
+  def createKey(s:String) = s"pref_$s"
+
   def getSound = {
-    true
+    store match {
+      case Some(s) => s.getBoolean(createKey("sound"), true)
+      case None => true
+    }
   }
   
-  def setSound(sound:Int) = {
-    
+  def setSound(snd:Int) = {
+    store map { s => 
+      val editor = s.edit
+      editor.putBoolean(createKey("sound"), snd == 1)
+      editor.commit
+    }
+
+    // update local cache
+    cache = cache map { p => p.copy(sound = snd == 1) }
   }
   
   def getVibrate = {
-    2
+    store match {
+      case Some(s) => s.getInt(createKey("vibrate"), 1)
+      case None => 1
+    }
   }
   
-  def setVibrate(vibrate:Int) = {
+  def setVibrate(vib:Int) = {
+    store map { s =>
+      val editor = s.edit
+      editor.putInt(createKey("vibrate"), vib)
+      editor.commit
+    }
     
+    cache = cache map { p => p.copy(vibrate = vib)}
   }
   
 }
