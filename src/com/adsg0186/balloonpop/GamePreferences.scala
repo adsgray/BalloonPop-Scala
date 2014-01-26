3b91afb071cdbc8d6a0359d37131ec537602da5b
Andrew Gray <adsgray@gmail.com>
2014-01-26 08:40:59 -0800

    preferences that do not save (yet)

---
+++ src/com/adsg0186/balloonpop/GamePreferences.scala
@@ -0,0 +1,21 @@
+package com.adsg0186.balloonpop
+
+object GamePreferences {
+
+  def getSound = {
+    true
+  }
+  
+  def setSound(sound:Int) = {
+    
+  }
+  
+  def getVibrate = {
+    2
+  }
+  
+  def setVibrate(vibrate:Int) = {
+    
+  }
+  
+}
--- src/com/adsg0186/balloonpop/SettingsActivity.scala
+++ src/com/adsg0186/balloonpop/SettingsActivity.scala
@@ -2,12 +2,62 @@ package com.adsg0186.balloonpop
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.View
+import android.widget.RadioButton
+import android.widget.CheckBox
 
 class SettingsActivity extends Activity with ActivityUtil {
 
   override def onCreate(savedInstanceState:Bundle) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_settings);
+    setContentView(R.layout.activity_settings)
+    initVibrateRadioButtons()
+    initSoundCheckBox()
   }
+  
+  /*
+  override def onResume = {
+    super.onResume
+    initVibrateRadioButtons()
+    initSoundCheckBox()
+  }
+  * *
+  */
 
+  def onRadioButtonClicked(rb:View) = {
+    // ugh
+    val checked = rb.asInstanceOf[RadioButton].isChecked
+    
+    val vibratePref = rb.getId() match {
+      case R.id.vibrate_off => 0
+      case R.id.vibrate_low => 1
+      case R.id.vibrate_high => 2
+    }
+    
+    if (checked) {
+      GamePreferences.setVibrate(vibratePref)
+    }
+  }
+  
+  // set sound checkbox state from saved preferences
+  def initSoundCheckBox() = findView[CheckBox](R.id.soundcheckbox).setChecked(GamePreferences.getSound)
+  
+  // set the radio button state from saved preferences
+  def initVibrateRadioButtons() = {
+    //val vibratePref = GamePreferences.getVibrate
+
+    // disable all radio buttons
+    List(R.id.vibrate_off, R.id.vibrate_low, R.id.vibrate_high) map { 
+      id => findView[RadioButton](id).setChecked(false)
+    }
+    
+    // enable the one from preferences
+    val rbId = GamePreferences.getVibrate match {
+      case 0 => R.id.vibrate_off
+      case 1 => R.id.vibrate_low
+      case 2 => R.id.vibrate_high
+    }
+    
+    findView[RadioButton](rbId).setChecked(true)
+  }
 }
