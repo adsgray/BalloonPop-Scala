33fa85728cf9b784a91922bdc922ae1d3a383dfe
Andrew Gray <adsgray@gmail.com>
2014-02-01 08:47:54 -0800

    fix achievement caching. pointsperpin leaderboard mysteriously doesn't work though

--- src/com/adsg0186/balloonpop/AchievementTracker.scala
+++ src/com/adsg0186/balloonpop/AchievementTracker.scala
@@ -1,10 +1,10 @@
 package com.adsg0186.balloonpop
 
 import com.google.android.gms.games.GamesClient
-
 import android.content.Context
 import android.content.SharedPreferences
 import android.preference.PreferenceManager
+import android.util.Log
 
 object AchievementTracker {
   // use sharedpreferences to store
@@ -15,9 +15,10 @@ object AchievementTracker {
 
   var store:Option[SharedPreferences] = None
   var c:Context = null // ugh
-  var dirty:Boolean = false
+  var dirty:Boolean = true
   
   def init(c:Context) = {
+    Log.d("scores", "AchievementTracker initialized")
     store = Some(PreferenceManager.getDefaultSharedPreferences(c))
     this.c = c
   }
@@ -25,12 +26,14 @@ object AchievementTracker {
   def key(s:String) = s"achievement_$s"
   
   def incGamesPlayed = store map { s =>
+    Log.d("scores", "inc games played")
     val k = key("gamesplayed")
     val played = s.getInt(k, 0)
     s.edit.putInt(k, played + 1).commit
   }
   
   def saveScore(score:Int) = store map { s =>
+    Log.d("scores", s"save score $score")
     val k = key("highscore")
     val highscore = s.getInt(k, 0)
     if (score > highscore) {
@@ -39,6 +42,7 @@ object AchievementTracker {
   }
   
   def saveScorePerPin(spp:Float) = store map { s =>
+    Log.d("scores", f"save scoreperpin $spp%.2f")
     val k = key("scoreperpin")
     val prev = s.getFloat(k, 0.0f)
     if (spp > prev) {
@@ -50,30 +54,32 @@ object AchievementTracker {
   // must match achievements set up on the Play store
   def processScore(score:Int) = store map { s =>
     
+    Log.d("scores", s"process score $score")
     if (score <= 10) {
-      s.edit.putBoolean(c.getResources().getString(R.string.achievement_wallflower), true)
+      s.edit.putBoolean(c.getResources().getString(R.string.achievement_wallflower), true).commit
     }
     
     // see processScorePerPin for why these are in reverse order with if-elses
     if (score >= 50000) {
-      s.edit.putBoolean(c.getResources().getString(R.string.achievement_score50000), true)
+      s.edit.putBoolean(c.getResources().getString(R.string.achievement_score50000), true).commit
     }
     else if (score >= 30000) {
-      s.edit.putBoolean(c.getResources().getString(R.string.achievement_score30000), true)
+      s.edit.putBoolean(c.getResources().getString(R.string.achievement_score30000), true).commit
     }
     else if (score >= 20000) {
-      s.edit.putBoolean(c.getResources().getString(R.string.achievement_score20000), true)
+      s.edit.putBoolean(c.getResources().getString(R.string.achievement_score20000), true).commit
     }
     else if (score >= 10000) {
-      s.edit.putBoolean(c.getResources().getString(R.string.achievement_score10000), true)
+      s.edit.putBoolean(c.getResources().getString(R.string.achievement_score10000), true).commit
     }
   }
   
   // set boolean achievements
   // must match achievements set up on the Play store
   def processScorePerPin(spp:Float) = store map { s =>
+    Log.d("scores", f"process score per pin $spp%.2f")
     if (spp < 10.0f) {
-      s.edit.putBoolean(c.getResources().getString(R.string.achievement_rapidpin), true)
+      s.edit.putBoolean(c.getResources().getString(R.string.achievement_rapidpin), true).commit
     }
  
     // give these out in reverse order.
@@ -82,34 +88,38 @@ object AchievementTracker {
     // You'll get the lower ones when you achieve them specifically...
     // The "else ifs" achieve this.
     if (spp >= 150.0f) {
-      s.edit.putBoolean(c.getResources().getString(R.string.achievement_ultrapin), true)
+      s.edit.putBoolean(c.getResources().getString(R.string.achievement_ultrapin), true).commit
     }
     else if (spp >= 100.0f) {
-      s.edit.putBoolean(c.getResources().getString(R.string.achievement_propin), true)
+      s.edit.putBoolean(c.getResources().getString(R.string.achievement_propin), true).commit
     }
     else if (spp >= 75.0f) {
-      s.edit.putBoolean(c.getResources().getString(R.string.achievement_greatpin), true)
+      s.edit.putBoolean(c.getResources().getString(R.string.achievement_greatpin), true).commit
     }
     else if (spp >= 50.0f) {
-      s.edit.putBoolean(c.getResources().getString(R.string.achievement_goodpin), true)
+      s.edit.putBoolean(c.getResources().getString(R.string.achievement_goodpin), true).commit
     }
 
   }
   
   def processGamesPlayed = store map { s =>
+    Log.d("scores", "process games played")
     val k = key("gamesplayed")
     val played = s.getInt(k, 0)
     
+    Log.d("scores", s"games played=$played")
+    
     if (played >= 10) {
-      s.edit.putBoolean(c.getResources().getString(R.string.achievement_played10), true)
+      s.edit.putBoolean(c.getResources().getString(R.string.achievement_played10), true).commit
     }
 
     if (played >= 100) {
-      s.edit.putBoolean(c.getResources().getString(R.string.achievement_played100), true)
+      s.edit.putBoolean(c.getResources().getString(R.string.achievement_played100), true).commit
     }
   }
   
   def gameCompleted(score:Int, scorePerPin:Float) {
+    Log.d("scores", "game completed")
     incGamesPlayed
     saveScore(score)
     saveScorePerPin(scorePerPin)
@@ -122,9 +132,15 @@ object AchievementTracker {
   
   def saveToGoogle(gc:GamesClient) = store map { s => 
     if (dirty) {
+      Log.d("scores", "scores dirty, submitting")
+
       // submit scores
-      gc.submitScore(c.getResources.getString(R.string.leaderboard_bestround), s.getInt(key("highscore"), 0))
+      val score = s.getInt(key("highscore"), 0)
+      Log.d("scores", s"submitting score=$score")
+      gc.submitScore(c.getResources.getString(R.string.leaderboard_bestround), score)
+
       val perpinAsInt = (s.getFloat(key("scoreperpin"), 0.0f) * 100).round
+      Log.d("scores", s"submitting pointsperpin=$perpinAsInt")
       gc.submitScore(c.getResources.getString(R.string.leaderboard_pointsperpin), perpinAsInt)
       
       // unlock achievements
@@ -143,6 +159,7 @@ object AchievementTracker {
           c.getResources.getString(R.string.achievement_score50000)
           ) map { achString =>
             if (s.getBoolean(achString, false)) {
+              Log.d("scores", s"unlocking achievement $achString")
               gc.unlockAchievement(achString)
             }
       }
--- src/com/adsg0186/balloonpop/MainActivity.scala
+++ src/com/adsg0186/balloonpop/MainActivity.scala
@@ -6,9 +6,13 @@ import android.widget.Button
 import com.adsg0186.balloonpop.implicitOps._
 import com.google.example.games.basegameutils.BaseGameActivity
 import android.view.View
+import android.util.Log
 
 class MainActivity extends BaseGameActivity with ActivityUtil {
 
+  // not really used
+  private val REQUEST_LEADERBOARD = 1
+  private val REQUEST_ACHIEVEMENTS = 2
 
   val googleButtonHandler = ClickListener { v => 
     v.getId() match {
@@ -27,27 +31,58 @@ class MainActivity extends BaseGameActivity with ActivityUtil {
     
     findView[Button](R.id.play_button).onClick { v => goToActivity(classOf[GDXActivity])}
     findView[Button](R.id.settings_button).onClick { v => goToActivity(classOf[SettingsActivity])}
+
     findView[Button](R.id.sign_in_button).setOnClickListener(googleButtonHandler)
     findView[Button](R.id.sign_out_button).setOnClickListener(googleButtonHandler)
+    
+    findView[Button](R.id.points_leaderboard).onClick { 
+      v => startActivityForResult(getGamesClient.getLeaderboardIntent(getResources.getString(R.string.leaderboard_bestround)), REQUEST_LEADERBOARD)
+    }
+
+    findView[Button](R.id.pointsperpin_leaderboard).onClick { 
+      v => startActivityForResult(getGamesClient.getLeaderboardIntent(getResources.getString(R.string.leaderboard_pointsperpin)), REQUEST_LEADERBOARD)
+    }
+
+    findView[Button](R.id.achievements).onClick { 
+      v => startActivityForResult(getGamesClient.getAchievementsIntent(), REQUEST_ACHIEVEMENTS)
+    }
     
     GamePreferences.init(getApplicationContext)
     AchievementTracker.init(getApplicationContext)
   }
   
+  /*
   override def onResume = {
     super.onResume
     AchievementTracker.saveToGoogle(getGamesClient)
   }
+  * 
+  */
   
-   def onSignInSucceeded(): Unit = {
+  def signedInButtons = {
      findView[Button](R.id.sign_in_button).setVisibility(View.GONE)
      findView[Button](R.id.sign_out_button).setVisibility(View.VISIBLE)
+     findView[Button](R.id.points_leaderboard).setVisibility(View.VISIBLE)
+     findView[Button](R.id.pointsperpin_leaderboard).setVisibility(View.VISIBLE)
+     findView[Button](R.id.achievements).setVisibility(View.VISIBLE)
+  }
+  
+  def signedOutButtons = {
+     findView[Button](R.id.sign_in_button).setVisibility(View.VISIBLE)
+     findView[Button](R.id.sign_out_button).setVisibility(View.GONE)
+     findView[Button](R.id.points_leaderboard).setVisibility(View.GONE)
+     findView[Button](R.id.pointsperpin_leaderboard).setVisibility(View.GONE)
+     findView[Button](R.id.achievements).setVisibility(View.GONE)
+  }
+
+   def onSignInSucceeded(): Unit = {
+     signedInButtons
      // (your code here: update UI, enable functionality that depends on sign in, etc)
-     // TODO: make visible button to view high scores
+     Log.d("scores", "submitting scores")
+     AchievementTracker.saveToGoogle(getGamesClient)
    }
 
    def onSignInFailed(): Unit = {
-     findView[Button](R.id.sign_in_button).setVisibility(View.VISIBLE)
-     findView[Button](R.id.sign_out_button).setVisibility(View.GONE)
+     signedOutButtons
    } 
 }
