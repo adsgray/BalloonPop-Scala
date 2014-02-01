6fc4170b7ffd36b5ed0e0a8b363823359cdbee40
Andrew Gray <adsgray@gmail.com>
2014-01-31 18:11:11 -0800

    stub functions for Google Game api

--- src/com/adsg0186/balloonpop/MainActivity.scala
+++ src/com/adsg0186/balloonpop/MainActivity.scala
@@ -4,8 +4,9 @@ import android.app.Activity
 import android.os.Bundle
 import android.widget.Button
 import com.adsg0186.balloonpop.implicitOps._
+import com.google.example.games.basegameutils.BaseGameActivity
 
-class MainActivity extends Activity with ActivityUtil {
+class MainActivity extends BaseGameActivity with ActivityUtil {
 
   override def onCreate(savedInstanceState:Bundle) = {
     super.onCreate(savedInstanceState)
@@ -16,4 +17,9 @@ class MainActivity extends Activity with ActivityUtil {
     
     GamePreferences.init(getApplicationContext)
   }
+  
+  
+   def onSignInFailed(): Unit = ??? 
+
+   def onSignInSucceeded(): Unit = ???
 }
