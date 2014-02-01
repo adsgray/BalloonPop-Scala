0130ad34cc1476cc6ea904e491dde3bd4f927929
Andrew Gray <adsgray@gmail.com>
2014-01-31 20:32:13 -0800

    initial Google Game Services sign in

--- src/com/adsg0186/balloonpop/MainActivity.scala
+++ src/com/adsg0186/balloonpop/MainActivity.scala
@@ -5,8 +5,21 @@ import android.os.Bundle
 import android.widget.Button
 import com.adsg0186.balloonpop.implicitOps._
 import com.google.example.games.basegameutils.BaseGameActivity
+import android.view.View
 
 class MainActivity extends BaseGameActivity with ActivityUtil {
+
+
+  val googleButtonHandler = ClickListener { v => 
+    v.getId() match {
+      case R.id.sign_in_button => 
+        beginUserInitiatedSignIn
+      case R.id.sign_out_button =>
+        signOut
+        findView[Button](R.id.sign_in_button).setVisibility(View.VISIBLE)
+        findView[Button](R.id.sign_out_button).setVisibility(View.GONE)
+    }
+  }
 
   override def onCreate(savedInstanceState:Bundle) = {
     super.onCreate(savedInstanceState)
@@ -14,12 +27,22 @@ class MainActivity extends BaseGameActivity with ActivityUtil {
     
     findView[Button](R.id.play_button).onClick { v => goToActivity(classOf[GDXActivity])}
     findView[Button](R.id.settings_button).onClick { v => goToActivity(classOf[SettingsActivity])}
+    findView[Button](R.id.sign_in_button).setOnClickListener(googleButtonHandler)
+    findView[Button](R.id.sign_out_button).setOnClickListener(googleButtonHandler)
     
     GamePreferences.init(getApplicationContext)
   }
   
   
-   def onSignInFailed(): Unit = ??? 
+   def onSignInSucceeded(): Unit = {
+     findView[Button](R.id.sign_in_button).setVisibility(View.GONE)
+     findView[Button](R.id.sign_out_button).setVisibility(View.VISIBLE)
+     // (your code here: update UI, enable functionality that depends on sign in, etc)
+     // TODO: make visible button to view high scores
+   }
 
-   def onSignInSucceeded(): Unit = ???
+   def onSignInFailed(): Unit = {
+     findView[Button](R.id.sign_in_button).setVisibility(View.VISIBLE)
+     findView[Button](R.id.sign_out_button).setVisibility(View.GONE)
+   } 
 }
